c196fbca416ed9d69fc3755bac618355112430f2
Amogh Gupta <amogh17009@cse.ssn.edu.in>
2021-01-29 13:19:19 +0530

    add tests and update factorial compute mod 1e9+7

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,18 +4,17 @@ object Main extends App {
 
   @tailrec def factorial (x: Long, acc:Long = 1): Long = {
 
-    if (x < 0) {
+    if (x < 0)
       -1
-    }
 
-    else if (x <= 1) {
+    else if (x <= 1)
       acc
-    }
 
     else
-      factorial(x-1, acc*x)
+      factorial(x-1, (acc*x)%prime)
   }
 
   var number: Int = scala.io.StdIn.readInt()
+  var prime: Long = (1e9+7).toLong
   println (factorial(number.toLong))
 }
--- src/test/scala/MainTest.scala
+++ src/test/scala/MainTest.scala
@@ -2,6 +2,9 @@ import org.scalatest.FunSuite
 
 object MainTest  extends FunSuite {
   test ("Main.factorial") {
-    assert(Main.factorial(1) === 1)
+    assert (Main.factorial(1) === 1)
+    assert (Main.factorial(0) === 1)
+    assert (Main.factorial(3) === 6)
+    assert (Main.factorial(9) === 362880)
   }
 }
