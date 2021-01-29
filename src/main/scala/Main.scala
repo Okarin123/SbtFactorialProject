657acc52b74026a8730d6ab2d418169394f361f9
Amogh Gupta <amogh17009@cse.ssn.edu.in>
2021-01-29 11:41:17 +0530

    update factorial to tail recursion from recursion.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,16 +1,19 @@
+import scala.annotation.tailrec
+
 object Main extends App {
 
-  def factorial (x: Long): Long = {
+  @tailrec def factorial (x: Long, acc:Long = 1): Long = {
 
     if (x < 0) {
-      return -1
+      -1
     }
 
-    if (x <= 1) {
-      return 1
+    else if (x <= 1) {
+      acc
     }
 
-    return x*factorial(x-1)
+    else
+      factorial(x-1, acc*x)
   }
 
   var number: Int = scala.io.StdIn.readInt()
